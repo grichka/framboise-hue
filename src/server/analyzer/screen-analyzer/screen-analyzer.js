929feec39f99dc674d42b85c36f536b24ac88cdf
grichka <thomas.moreau.f@gmail.com>
2017-02-28 01:50:20 +0100

    #6 - Screen average with threshold and saturation algorithm - first version of the algorithm (brightness is not working fine)

--- src/server/analyzer/screen-analyzer/screen-analyzer.js
+++ src/server/analyzer/screen-analyzer/screen-analyzer.js
@@ -162,6 +162,10 @@ function ScreenAnalyzer(config) {
                                 g += imageData.data[i + 1];
                                 b += imageData.data[i + 2];
                             }
+                            // avoid dividing by zero
+                            if(pixelCount === 0) {
+                                pixelCount++;
+                            }
 
                             var rAvg = (r / pixelCount) >> 0,// jshint ignore:line
                                 gAvg = (g / pixelCount) >> 0,// jshint ignore:line
@@ -180,6 +184,108 @@ function ScreenAnalyzer(config) {
                 });
             });
         } catch (e) {
+            deferred.reject(e);
+        }
+
+        return deferred.promise;
+    };
+
+    /**
+     * Get the colors to apply to each light corresponding to screen average color.
+     * Uses thresholds and calculates dark pixels ratio to determine the brightness to apply.
+     */
+    this.getScreenBetterAverageColor = function() {
+        var deferred = q.defer();
+        var that = this;
+
+        try {
+            var screenshotName = getScreenshotName('screenshotBetterAverageColor');
+            var nircmd = childProcess.spawn(nircmdPath, ['savescreenshot', screenshotName, that.x, that.y, that.width, that.height]);
+            nircmd.on('close', function (/*code, signal*/) {
+                fs.readFile(screenshotName, function (err, screenshotBuffer) {
+                    if(err) {
+                        deferred.reject(err);
+                    } else {
+                        var img = new Image();
+                        img.src = screenshotBuffer;
+                        that.ctx.drawImage(img, 0, 0, that.canvasWidth, that.canvasHeight);
+
+                        var result = [];
+
+                        // calculate avg color of all pixels and ratio of each zone
+                        that.lights.forEach(function(light) {
+                            var r = 0,
+                                g = 0,
+                                b = 0,
+                                darkPixels = 0,
+                                midDarkPixels = 0,
+                                pixelCount = 0;
+                            var imageData;
+                            if(that.useZones) {
+                                imageData = that.ctx.getImageData((light.zone.x * that.canvasXScale) >> 0, (light.zone.y * that.canvasYScale) >> 0, (light.zone.width * that.canvasXScale) >> 0, (light.zone.height * that.canvasYScale) >> 0);//jshint ignore:line
+                            } else {
+                                imageData = that.ctx.getImageData((that.x * that.canvasXScale) >> 0, (that.y * that.canvasYScale) >> 0, (that.width * that.canvasXScale) >> 0, (that.height * that.canvasYScale) >> 0);//jshint ignore:line
+                            }
+                            var imageDataLength = imageData.data.length;
+
+                            var i;
+                            for(i = 0 ; i < imageDataLength ; i += 4) {
+                                ++pixelCount;
+                                
+                                var red = imageData.data[i],
+                                    green = imageData.data[i + 1],
+                                    blue = imageData.data[i + 2];
+                                
+                                // don't count pixels that are too dark
+                                if(red < that.lowThreshold && green < that.lowThreshold && blue < that.lowThreshold) {
+                                    ++darkPixels;
+                                }
+                                // don't count pixels that are too light
+                                else if (red > that.highThreshold && green > that.highThreshold && blue > that.highThreshold) {
+                                    // nothing
+                                } else {
+                                    if(red < that.midThreshold && green < that.midThreshold && blue < that.midThreshold) {
+                                        ++midDarkPixels;
+                                        ++darkPixels;
+                                    }
+                                    r += red;
+                                    g += green;
+                                    b += blue;
+                                }
+                            }
+
+                            var rAvg = (r / pixelCount) >> 0,// jshint ignore:line
+                                gAvg = (g / pixelCount) >> 0,// jshint ignore:line
+                                bAvg = (b / pixelCount) >> 0;// jshint ignore:line
+                            
+                            var color = [
+                                rAvg < that.lowThreshold ? that.lowThreshold : rAvg,
+                                gAvg < that.lowThreshold ? that.lowThreshold : gAvg,
+                                bAvg < that.lowThreshold ? that.lowThreshold : bAvg
+                            ];
+
+                            // var darkRatio = (darkPixels / pixelCount) * 100
+                            var brightness = ((((darkPixels * light.maxBrightness - 1) * (light.maxBrightness - light.minBrightness)) / ((light.maxBrightness - 1) || 1)) + light.minBrightness + 1) >> 0; // jshint ignore:line
+
+                            if(brightness < light.minBrightness) {
+                                brightness = light.minBrightness;
+                            } else if(brightness > light.maxBrightness) {
+                                brightness = light.maxBrightness;
+                            }
+
+                            result.push({
+                                id: light.id,
+                                color: color,
+                                brightness: brightness,
+                                transition: light.transition || 0.7
+                            });
+                        });
+
+                        deferred.resolve(result);
+                    }
+                });
+            });
+        } catch(e) {
             deferred.reject(e);
         }
 
